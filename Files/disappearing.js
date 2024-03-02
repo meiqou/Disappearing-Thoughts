9b84aca16f78b5ac90fabb4b22577db3561f3d31
Glory Mae Plateros <158459152+meiqou@users.noreply.github.com>
2024-03-02 17:42:29 +0800

    disappearing.js

--- Files/disappearing.js
+++ Files/disappearing.js
@@ -1,5 +1,35 @@
-// Initialize Firebase Realtime Database
-const database = firebase.database();
+const firebaseConfig = {
+  apiKey: "AIzaSyAep1mcceiSO0S2ylum-zpzoD58Fx-rcpY",
+  authDomain: "disappearing-thoughts.firebaseapp.com",
+  databaseURL: "https://disappearing-thoughts-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "disappearing-thoughts",
+  storageBucket: "disappearing-thoughts.appspot.com",
+  messagingSenderId: "92776706019",
+  appId: "1:92776706019:web:3e9a9c76bf418148dd7ad6",
+  measurementId: "G-SKK4SRE5P6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//reference to database
+var chatFormDB = firebase.database().ref(chatForm);
+
+document.getElementById('chatForm').addEventListener('submit' , submitForm);
+
+function submitForm(e){
+  e.preventDefault();
+  var text = getElementVal('text');
+  var emailid = getElementVal
+
+}
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+}
+
+
+
 
 // Function to append a new chat message to Firebase
 function appendMessageToFirebase(message) {
@@ -39,3 +69,4 @@ document.getElementById('chat-form').addEventListener('submit', function(event) 
 window.onload = function() {
   fetchChatMessagesFromFirebase();
 };
+
